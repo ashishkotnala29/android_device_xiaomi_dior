d1debf8d0644ff67c34fa25a943f4e2f5224fb6d
Ashish Kotnala <ashishkotnala1.ak@gmail.com>
2017-10-08 13:12:37 +0000

    dior: sync BT with caf

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,11 +18,15 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
+#include <cutils/properties.h>
+#include <string.h>
+
 #define BTM_DEF_LOCAL_NAME   "HM NOTE 1LTE"
 
 #define BLUETOOTH_QTI_SW TRUE
-
-/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
-#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
+#define MAX_ACL_CONNECTIONS   7
+#define MAX_L2CAP_CHANNELS    16
+#define BLE_VND_INCLUDED   TRUE
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
 
 #endif
