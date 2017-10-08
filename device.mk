a05b9c3148eec42aba27a9e50a83fc3e48c97ae3
Ashish Kotnala <ashishkotnala1.ak@gmail.com>
2017-10-08 12:31:41 +0000

    dior: Update audio for O
    * Build audio HALs
    * Remove dead packages
    * Update audio flags for AOSP HAL

--- device.mk
+++ device.mk
@@ -42,7 +42,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    audiod \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio.primary.msm8226 \
     audio.r_submix.default \
